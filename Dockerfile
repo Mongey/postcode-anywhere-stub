bff72ce2b31329e0dccfb3487857a223e862c3f7
Conor Mongey <conor@mongey.net>
2017-10-04 12:05:12 +0100

    Add BankAcoountValidation Endpoint

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
-FROM golang:1.6-onbuild
+FROM alpine:latest
 
-RUN openssl genrsa -out /go/src/app/server.key 2048 && echo "GB\n\n\n\n\n\n" | openssl req -new -x509 -sha256 -key /go/src/app/server.key -out /go/src/app/server.pem -days 3650
+WORKDIR /app
+COPY postcode-anywhere-stub .
 
 EXPOSE 9040
+
+ENTRYPOINT ["/app/postcode-anywhere-stub"]
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
-package postcode_anywhere_stub
+package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
-	"flag"
+
+	"github.com/icrowley/fake"
 )
 
 var port, cert, key string
 
 type AddressList struct {
 	Items []Address
+}
+type BankList struct {
+	Items []BankAccountResponse
 }
 
 type Address struct {
@@ -65,8 +70,28 @@ type Item struct {
 	Description string
 }
 
-func findHandler(w http.ResponseWriter, r *http.Request) {
+type BankAccountResponse struct {
+	IsCorrect               bool   `json:"is_correct"`
+	IsDirectDebitCapable    bool   `json:"is_direct_debit_capable"`
+	StatusInformation       string `json:"status_information"`
+	CorrectedSortCode       string `json:"corrected_sort_code"`
+	CorrectedAccountNumber  string `json:"corrected_account_number"`
+	IBAN                    string `json:"iban"`
+	Bank                    string `json:"bank"`
+	BankBIC                 string `json:"bank_bic"`
+	Branch                  string `json:"branch"`
+	BranchBIC               string `json:"branch_bic"`
+	ContactAddressLine1     string `json:"contact_address_line1"`
+	ContactAddressLine2     string `json:"contact_address_line2"`
+	ContactPostTown         string `json:"contact_post_town"`
+	ContactPostcode         string `json:"contact_postcode"`
+	ContactPhone            string `json:"contact_phone"`
+	ContactFax              string `json:"contact_fax"`
+	FasterPaymentsSupported bool   `json:"faster_payments_supported"`
+	ChapsSupported          bool   `json:"chaps_supported"`
+}
 
+func findHandler(w http.ResponseWriter, r *http.Request) {
 	items := ItemList{
 		Items: make([]Item, 0),
 	}
@@ -108,7 +133,6 @@ func findHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func retrieveHandler(w http.ResponseWriter, r *http.Request) {
-
 	items := AddressList{
 		Items: make([]Address, 0),
 	}
@@ -159,6 +183,32 @@ func retrieveHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(items)
 }
 
+func bankAccountHandler(w http.ResponseWriter, r *http.Request) {
+	sortCode := r.URL.Query().Get("SortCode")
+	accountNumber := r.URL.Query().Get("AccountNumber")
+
+	response := BankAccountResponse{
+		IsCorrect:               true,
+		IsDirectDebitCapable:    true,
+		CorrectedSortCode:       sortCode,
+		CorrectedAccountNumber:  accountNumber,
+		ContactAddressLine1:     fake.StreetAddress(),
+		ContactAddressLine2:     fake.City(),
+		ContactPhone:            fake.Phone(),
+		ContactFax:              fake.Phone(),
+		ChapsSupported:          true,
+		FasterPaymentsSupported: true,
+	}
+	items := BankList{
+		Items: []BankAccountResponse{response},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "pca-source")
+	json.NewEncoder(w).Encode(items)
+}
+
 func init() {
 	flag.StringVar(&port, "port", "9040", "Port number to bind onto")
 	flag.StringVar(&cert, "cert", "server.pem", "Server certificate")
@@ -169,7 +219,8 @@ func init() {
 func main() {
 	http.HandleFunc("/Capture/Interactive/Find/v1.00/json3ex.ws", findHandler)
 	http.HandleFunc("/Capture/Interactive/Retrieve/v1.00/json3ex.ws", retrieveHandler)
-	err := http.ListenAndServeTLS(":" + port, cert, key, nil)
+	http.HandleFunc("/BankAccountValidation/Interactive/Validate/v2.00/json3.ws", bankAccountHandler)
+	err := http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
